4d3da3859f19907df05808dc56e306c8fd95e4ce
OlgaLokhtina <stella_3_@mail.ru>
2025-03-05 15:55:30 +0300

    issues/Add generic type

--- src/datastructures/stack.py
+++ src/datastructures/stack.py
@@ -1,15 +1,61 @@
-class Stack:
-    def __init__(self, st: list):
+from typing import Any
+
+
+class Stack[T]:
+    """
+    Class for creating Stack
+
+    ...
+
+    Attributes
+    ----------
+        st : any type
+            element of stack
+
+    Methods
+    -------
+        push(element: Any):
+            adds the element to the stack
+        pop():
+            removes the last element from the stack
+        peek():
+            returns the last element from stack
+        val():
+            returns all the elements of the stack
+    """
+    def __init__(self, st: list[T] = None):
         self.st = st
 
-    def push(self, element):
+    def push(self, element: Any):
+        """
+        Adds the element to the stack
+
+        :param element: new element for adding to stack
+        :type element: Any
+
+        :return: None
+        """
         self.st.append(element)
 
     def pop(self):
+        """
+        Removes the last element from the stack
+
+        :return: None
+        """
         self.st.pop()
 
-    def peek(self):
+    def peek(self) -> T:
+        """
+        Returns the last element from stack
+
+        :return: the last element
+        """
         return self.st[-1]
 
-    def val(self):
+    def val(self) -> list[T]:
+        """
+        Returns all the elements of the stack
+        :return: the list of stack elements
+        """
         return self.st
