e6c5c19e4bb7d5cbe230a173462afb28f2a4da3f
OlgaLokhtina <stella_3_@mail.ru>
2025-03-27 11:40:39 +0300

    issues/writing tests

--- src/datastructures/linked_list.py
+++ src/datastructures/linked_list.py
@@ -2,19 +2,86 @@ from typing import Self
 
 
 class Node[T]:
+    """
+    Class for creating Node
+
+    ...
+
+    Attributes
+    ----------
+        val : T
+            the value of the node
+        prev : Node
+            previous node
+        next : Node
+            next node
+
+    Methods
+    -------
+        add_prev(val: T):
+            adds the previous node with a given value
+        add_next(val: T):
+            adds the next node with a given value
+    """
     def __init__(self, val: T | None = None):
         self.val = val
         self.prev: Self = None
         self.next: Self = None
 
     def add_prev(self, val: T):
+        """
+        Adds the previous node with a given value
+
+        :param val: the value of the previous node
+        :type val: T
+
+        :return: None
+        """
         self.prev = Node(val)
 
     def add_next(self, val: T):
+        """
+        Adds the next node with a given value
+
+        :param val: the value of the next node
+        :type val: T
+
+        :return: None
+        """
         self.next = Node(val)
 
 
 class DoubleLinkedList[T]:
+    """
+    Class for creating double linked list
+
+    ...
+
+    Attributes
+    ----------
+        head : Node | None
+            the first node of the linked list with the value value or None
+        len : int
+            the length of the list - the number of list nodes
+
+    Methods
+    -------
+        get_first()
+            returns the first node of the list
+        get_last()
+            returns the last node of the list
+        add_first(val: T)
+            adds the node to the beginning of the list
+        add_last(val: T)
+            adds the node to the end of the list
+        pop_first()
+            removes and returns the first node of the list
+        pop_last()
+            removes and returns the last node of the list
+        present()
+            returns the list of all nodes
+
+    """
     def __init__(self, value: T | None = None):
         if value is None:
             self.head = None
@@ -24,12 +91,22 @@ class DoubleLinkedList[T]:
             self.len = 1
 
     def get_first(self) -> T | None:
+        """
+        Returns the first node of the list
+
+        :return: T | None
+        """
         if self.head:
             return self.head.val
         else:
             return None
 
     def get_last(self) -> T | None:
+        """
+        Returns the last node of the list
+
+        :return: T | None
+        """
         if self.head:
             a = self.head
             while a.next:
@@ -39,6 +116,14 @@ class DoubleLinkedList[T]:
             return None
 
     def add_first(self, val: T):
+        """
+        Adds the node to the beginning of the list
+
+        :param val: the value of the added node
+        :type val: T
+
+        :return: None
+        """
         if self.head is None:
             self.head = Node(val)
         else:
@@ -50,6 +135,14 @@ class DoubleLinkedList[T]:
         self.len += 1
 
     def add_last(self, val: T):
+        """
+        Adds the node to the end of the list
+
+        :param val: the value of the added node
+        :type val: T
+
+        :return: None
+        """
         if self.head is None:
             self.head = Node(val)
         else:
@@ -61,6 +154,11 @@ class DoubleLinkedList[T]:
         self.len += 1
 
     def pop_first(self) -> T:
+        """
+        Removes and returns the first node of the list
+
+        :return: T
+        """
         if self.head:
             r = self.head.val
             self.head = self.head.next
@@ -70,6 +168,11 @@ class DoubleLinkedList[T]:
             return None
 
     def pop_last(self) -> T:
+        """
+        Removes and returns the last node of the list
+
+        :return: T
+        """
         if self.head:
             a = self.head
             while a.next:
@@ -84,7 +187,12 @@ class DoubleLinkedList[T]:
         else:
             return None
 
-    def present(self):
+    def present(self) -> list | None:
+        """
+        Returns the list of all nodes
+
+        :return: list | None
+        """
         if self.head:
             s = [self.head.val]
             a = self.head
