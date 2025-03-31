a5f83bfe82397738b1789f90a56e86b187e30b99
OlgaLokhtina <stella_3_@mail.ru>
2025-03-31 11:51:08 +0300

    create binary tree

---
+++ src/datastructures/binary_tree.py
@@ -0,0 +1,51 @@
+from typing import Self
+
+
+class Node[T]:
+    def __init__(self, val: T):
+        self.val = val
+        self.right = None
+        self.left = None
+
+    def add_right(self, val: T):
+        self.right = Node(val)
+
+    def add_left(self, val: T):
+        self.left = Node(val)
+
+
+class Tree[T]:
+    def __init__(self, head: Node | None = None):
+        self.head = head
+
+    def present_tree(self):
+        print(self.head.val)
+        aa = self.head
+
+        def pr(a):
+            if a.left:
+                print(a.left.val)
+                pr(a.left)
+            if a.right:
+                print(a.right.val)
+                pr(a.right)
+        pr(aa)
+
+    @classmethod
+    def from_list(cls, list_value: list[T]) -> Self:
+        def create_tree(i):
+            if i < len(list_value):
+                h = Node(list_value[i])
+                if i*2+1 < len(list_value):
+                    h.left = create_tree(i*2+1)
+                if i*2+2 < len(list_value):
+                    h.right = create_tree(i*2+2)
+                return h
+        head = create_tree(0)
+        a = Tree(head)
+        return a
+
+
+l = [1, 2, 3, 4, 5, 6, 7]
+t = Tree.from_list(l)
+t.present_tree()
