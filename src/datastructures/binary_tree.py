0a53f5cac879ea6f81e3e119ddacfe5bfa443c4a
OlgaLokhtina <stella_3_@mail.ru>
2025-04-03 11:47:22 +0300

    issues/add tests

--- src/datastructures/binary_tree.py
+++ src/datastructures/binary_tree.py
@@ -19,47 +19,66 @@ class Tree[T]:
         self.head = head
 
     def present_tree(self):
-        print(self.head.val)
-        aa = self.head
+        if self.head:
+            print("Head -", self.head.val)
+            aa = self.head
 
-        def pr(a):
-            if a.left:
-                print(a.left.val)
-                pr(a.left)
-            if a.right:
-                print(a.right.val)
-                pr(a.right)
-        pr(aa)
+            def pr(a):
+                if a.left:
+                    print("Left -", a.left.val)
+                    pr(a.left)
+                if a.right:
+                    print("Right -", a.right.val)
+                    pr(a.right)
+            pr(aa)
+        print("Head -", self.head)
 
     @classmethod
     def from_list(cls, list_value: list[T]) -> Self:
-        n = [Node(el) for el in list_value]
-        tree = Tree(n[0])
-        m = 0
-        j = 1
-        while j < len(n):
-            k = m
-            m = j
-            for i in range(k, j):
-                if n[i].val:
-                    print("Node - ", n[i].val)
-                    if j < len(n):
-                        n[i].left = n[j]
-                        print(n[j].val)
-                        j += 1
-                    if j < len(n):
-                        n[i].right = n[j]
-                        print(n[j].val)
-                        j += 1
+        if list_value:
+            n = [Node(el) for el in list_value]
+            tree = Tree(n[0])
+            m = 0
+            j = 1
+            while j < len(n):
+                k = m
+                m = j
+                for i in range(k, j):
+                    if n[i].val:
+                        if j < len(n):
+                            n[i].left = n[j]
+                            j += 1
+                        if j < len(n):
+                            n[i].right = n[j]
+                            j += 1
+
+        else:
+            tree = Tree()
         return tree
 
     def to_list(self) -> list[T]:
-        pass
+        b = [self.head]
+        result = b
+        while b:
+            c = []
+            for el in b:
+                if el:
+                    c.append(el.left)
+                if el:
+                    c.append(el.right)
+            result.extend(c)
+            b = c
+        lst = [r.val for r in result if r]
+        return lst
 
 
-my_list = [5, 3, 7, 3, None, 8, 1, None, None, 4, 2]
+my_list = []
 t = Tree.from_list(my_list)
-print(t.present_tree())
+t.present_tree()
+nl = t.to_list()
+print(nl)
+
+
 
 
 
---
+++ src/tests/test_binary_tree.py
@@ -0,0 +1,47 @@
+import pytest
+
+from datastructures.binary_tree import Node, Tree
+
+
+@pytest.fixture()
+def new_binary_tree():
+    h = Node(1)
+    h.add_right(2)
+    h.add_left(3)
+    t = Tree(h)
+    return t
+
+
+@pytest.fixture()
+def new_list():
+    lst = [5, 4, 9, 1, 4, None, 10, 6, 9, 3, None]
+    return lst
+
+
+@pytest.fixture()
+def new_single_list():
+    lst = [5]
+    return lst
+
+
+@pytest.fixture()
+def new_empty_list():
+    empty_lst = []
+    return empty_lst
+
+
+def test_from_list_to_list(new_list):
+    tree = Tree.from_list(new_list)
+    assert tree.to_list() == new_list
+
+
+def test_from_single_list_to_list(new_single_list):
+    tree = Tree.from_list(new_single_list)
+    assert tree.to_list() == new_single_list
+
+
+def test_from_empty_list_to_list(new_empty_list):
+    empty_tree = Tree.from_list(new_empty_list)
+    assert empty_tree.to_list() == new_empty_list
+
+
